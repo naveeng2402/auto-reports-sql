e1df6ccc8182f71fe09504225aef37f713a0d95f
Naveen G <naveeng2402@gmail.com>
2022-11-02 14:47:27 +0530

    New folder structure with click

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="auto-reports-sql",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[
+        "click",
+    ],
+    entry_points="""
+    [console_scripts]
+    auto-reports-sql=auto_reports_sql:main
+    """,
+)
