0ea77173515e9ff6e1c1c1e6c35bd55055fcb75b
Naveen G <naveeng2402@gmail.com>
2022-11-03 10:48:31 +0530

    Updated setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,7 @@ setup(
     name="auto-reports-sql",
     version="0.0.1",
     packages=find_packages(),
-    install_requires=[
-        "click",
-    ],
+    install_requires=["click", "click-config-file", "jinja2"],
     entry_points="""
     [console_scripts]
     auto-reports-sql=auto_reports_sql:main
